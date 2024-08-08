108f63cd3cb1fb1d2536007a70faef8c5c2f2da5
Kishore Kalaiselvan <111948205+Kishore-Kalaiselvan@users.noreply.github.com>
2024-08-08 15:19:45 +0530

    Add files via upload

---
+++ NodeHealth.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+##########################################
+# Author : Kishore
+# Date : 08/08/24
+#
+# This Scripts outputs the node health
+#
+###########################################
+
+df -h
+
+free -g
+
+npro
