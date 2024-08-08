d303fd42168df9376ada28ac07b963cc0b411115
Kishore-Kalaiselvan <kishore.kalaiselvan003@gmail.com>
2024-08-08 10:22:31 +0000

    Sh File updated

---
+++ NodeHealth.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#################################################
+# Author : Kishore
+# Date : 08/08/24
+#
+# This Scripts outputs a node Health
+#################################################
+
+free
+
+df
+
+nproc
